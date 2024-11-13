6727c5443b25cd89ff809c447be55c1d089a5b13
Eliam2102 <157552853+Eliam2102@users.noreply.github.com>
2024-11-13 17:33:46 -0600

    Maquetación recuperada

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/NavBar/components/NavBar';
+import routes from './components/Routes/AppRoutes';
+import Footer from './components/Control/components/Footer';
+
+const ScrollToTop: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const mainElement = document.querySelector('main');
+    if (mainElement) {
+      mainElement.scrollTop = 0; // Reiniciar el scroll dentro del main
+    }
+  }, [location]);
+
+  return null;
+};
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      {/* El contenido principal (las rutas) se renderizan fuera del Navbar */}
+      <main>
+        <ScrollToTop /> {/* Este componente maneja el scroll */}
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </main>
+
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
